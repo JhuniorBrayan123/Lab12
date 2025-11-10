1a3454e437bd9eee422049277dcd44723b671392
Jhunior Brayan Gutierrez <gutierrezmamanib26@gmail.com>
2025-11-09 23:38:59 -0500

    Dibujar formas en el mapa

--- app/src/main/java/com/example/lab12_maps/MapScreen.kt
+++ app/src/main/java/com/example/lab12_maps/MapScreen.kt
@@ -32,6 +32,35 @@ fun MapScreen() {
             durationMs = 3000
         )
     }
+    // 📍 Otras ubicaciones
+    val locations = listOf(
+        LatLng(-16.433415, -71.5442652), // JLByR
+        LatLng(-16.4205151, -71.4945209), // Paucarpata
+        LatLng(-16.3524187, -71.5675994)  // Zamacola
+    )
+
+    // 🔺 Polígonos
+    val mallAventuraPolygon = listOf(
+        LatLng(-16.432292, -71.509145),
+        LatLng(-16.432757, -71.509626),
+        LatLng(-16.433013, -71.509310),
+        LatLng(-16.432566, -71.508853)
+    )
+
+    val parqueLambramaniPolygon = listOf(
+        LatLng(-16.422704, -71.530830),
+        LatLng(-16.422920, -71.531340),
+        LatLng(-16.423264, -71.531110),
+        LatLng(-16.423050, -71.530600)
+    )
+
+    val plazaDeArmasPolygon = listOf(
+        LatLng(-16.398866, -71.536961),
+        LatLng(-16.398744, -71.536529),
+        LatLng(-16.399178, -71.536289),
+        LatLng(-16.399299, -71.536721)
+    )
+
 
     Box(modifier = Modifier.fillMaxSize()) {
         // Añadir GoogleMap al layout
@@ -48,6 +77,7 @@ fun MapScreen() {
                     false
                 )
             )
+
 
             // Añadir marcador en Denver, Colorado
             Marker(
